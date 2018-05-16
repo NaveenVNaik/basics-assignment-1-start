176248ba1926d2e55795c5949babbaa42d8e8b57
Naveen <naveennaikhnr@gmail.com>
2018-05-16 19:02:03 +0530

    added all files

---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warningalert',
+    templateUrl:'./warningalert.component.html',
+    styles:[`
+        h3{
+            color:red;
+        }
+
+        .bgc{
+            background-color:mistyrose;
+        }
+    `]
+})
+
+export class WarningAlertComponent {}
